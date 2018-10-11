83a401aed04037ca03a83d77b3e8eaafd8fa5369
Dmitry <manxxeta@gmail.com>
2018-10-11 09:05:57 -0700

    editing

--- src/index.js
+++ src/index.js
@@ -4,29 +4,61 @@ import './index.css';
 
 class CalcBoard extends React.Component {
 
+    constructor() {
 
+        super();
+
+        this.state = {
+            calculations: ''
+        };
+
+        this.addValueToInput = this.addValueToInput.bind(this);
+
+    }
+
+
+    addValueToInput(event) {
+
+        // console.log(event.target.textContent)
+
+        // adding number to a field
+        // document.querySelector('.input-field').value += event.target.textContent;
+
+        let currentInputValue = document.querySelector('.input-field').value += event.target.textContent;
+        // let tempValueOne = 0;
+        // let tempValueTwo = 0;
+        // let tempString = '';
+
+
+        console.log(currentInputValue)
+
+        this.setState({
+            calculations: parseInt(currentInputValue)
+        });
+    }
 
     render(){
 
         return (
             <div className="calculator-board">
-                <input className="input-field"></input>
-                <div className="square-button"><h3>*</h3></div>
-                <div className="square-button"><h3>%</h3></div>
-                <div className="square-button"><h3>+</h3></div>
-                <div className="square-button"><h3>-</h3></div>
-                <div className="square-button"><h3>1</h3></div>
-                <div className="square-button"><h3>2</h3></div>
-                <div className="square-button"><h3>3</h3></div>
-                <div className="square-button"><h3>4</h3></div>
-                <div className="square-button"><h3>5</h3></div>
-                <div className="square-button"><h3>6</h3></div>
-                <div className="square-button"><h3>7</h3></div>
-                <div className="square-button"><h3>8</h3></div>
-                <div className="square-button"><h3>9</h3></div>
-                <div className="square-button"><h3>0</h3></div>
-                <div className="square-button"><h3>C</h3></div>
-                <div className="square-button"><h3>=</h3></div>
+
+                <input className="input-field" type="text" value={this.state.calculations} placeholder="0"></input>
+                <div className="square-button operations"><h3>*</h3></div>
+                <div className="square-button operations"><h3>%</h3></div>
+                <div className="square-button operations"><h3>+</h3></div>
+                <div className="square-button operations"><h3>-</h3></div>
+                <div className="square-button" onClick={this.addValueToInput}><h3>1</h3></div>
+                <div className="square-button" onClick={this.addValueToInput}><h3>2</h3></div>
+                <div className="square-button" onClick={this.addValueToInput}><h3>3</h3></div>
+                <div className="square-button" onClick={this.addValueToInput}><h3>4</h3></div>
+                <div className="square-button" onClick={this.addValueToInput}><h3>5</h3></div>
+                <div className="square-button" onClick={this.addValueToInput}><h3>6</h3></div>
+                <div className="square-button" onClick={this.addValueToInput}><h3>7</h3></div>
+                <div className="square-button" onClick={this.addValueToInput}><h3>8</h3></div>
+                <div className="square-button" onClick={this.addValueToInput}><h3>9</h3></div>
+                <div className="square-button" onClick={this.addValueToInput}><h3>0</h3></div>
+                <div className="square-button operations"><h3>C</h3></div>
+                <div className="square-button operations"><h3>=</h3></div>
          
             </div>
         )
