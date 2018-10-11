22fa9d81c5f160e3810eba8b93a54c1566f082f3
Dmitry <manxxeta@gmail.com>
2018-10-11 10:21:16 -0700

    editing

--- src/index.js
+++ src/index.js
@@ -2,97 +2,55 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+class CalcInputField extends React.Component {
+
+
+    render() {
+
+        return (
+            <input className="input-field" type="text" placeholder="0"></input>
+        );
+    }
+}
+
+
+class CalcButtons extends React.Component {
+
+    render() {
+        return (
+                <div className="square-button"><h3>*</h3></div>
+                <div className="square-button"><h3>%</h3></div>
+                <div className="square-button"><h3>+</h3></div>
+                <div className="square-button"><h3>-</h3></div>
+                <div className="square-button"><h3>1</h3></div>
+                <div className="square-button"><h3>2</h3></div>
+                <div className="square-button"><h3>3</h3></div>
+                <div className="square-button"><h3>4</h3></div>
+                <div className="square-button"><h3>5</h3></div>
+                <div className="square-button"><h3>6</h3></div>
+                <div className="square-button"><h3>7</h3></div>
+                <div className="square-button"><h3>8</h3></div>
+                <div className="square-button"><h3>9</h3></div>
+                <div className="square-button"><h3>0</h3></div>
+                <div className="square-button"><h3>C</h3></div>
+                <div className="square-button"><h3>=</h3></div>
+        );
+    }
+}
+
 class CalcBoard extends React.Component {
 
-    constructor() {
 
-        super();
-
-        this.state = {
-            currentValue: '',
-            firstValue: 0,
-            secondValue: 0,
-            operation: ''
-        };
-
-        this.addValueToInput = this.addValueToInput.bind(this);
-
-    }
-
-
-    addValueToInput(event) {
-
-        // adding number to a field
-        let currentInputValue = document.querySelector('.input-field').value += event.target.textContent;
- 
-        // setting currentValue in state to input field 
-        this.setState({
-            currentValue: parseInt(currentInputValue)
-        });
-    }
-
-    doOperation(event)
-    {
-
-        // save input value to a firstValue
-        // and clean current value
-        this.setState({
-            firstValue: this.state.firstValue + parseInt(this.state.currentValue),
-            currentValue: ''
-        })
-        
-        switch (event.target.textContent) {
-            case '*':
-                // clean currentValue
-                // save operation
-                break;
-            case '%':
-                
-                break;
-            case '+':
-                
-                break;
-            case '-':
-                
-                break;
-            case '=':
-                
-                break;
-            case 'C':
-                
-                break;
-        
-            default:
-                break;
-        }
-
-    }
-
+    
 
 
     render(){
 
         return (
             <div className="calculator-board">
-
-                <input className="input-field" type="text" value={this.state.calculations} placeholder="0"></input>
-                <div className="square-button" onClick={this.doOperation}><h3>*</h3></div>
-                <div className="square-button" onClick={this.doOperation}><h3>%</h3></div>
-                <div className="square-button" onClick={this.doOperation}><h3>+</h3></div>
-                <div className="square-button" onClick={this.doOperation}><h3>-</h3></div>
-                <div className="square-button" onClick={this.addValueToInput}><h3>1</h3></div>
-                <div className="square-button" onClick={this.addValueToInput}><h3>2</h3></div>
-                <div className="square-button" onClick={this.addValueToInput}><h3>3</h3></div>
-                <div className="square-button" onClick={this.addValueToInput}><h3>4</h3></div>
-                <div className="square-button" onClick={this.addValueToInput}><h3>5</h3></div>
-                <div className="square-button" onClick={this.addValueToInput}><h3>6</h3></div>
-                <div className="square-button" onClick={this.addValueToInput}><h3>7</h3></div>
-                <div className="square-button" onClick={this.addValueToInput}><h3>8</h3></div>
-                <div className="square-button" onClick={this.addValueToInput}><h3>9</h3></div>
-                <div className="square-button" onClick={this.addValueToInput}><h3>0</h3></div>
-                <div className="square-button" onClick={this.clearInputField}><h3>C</h3></div>
-                <div className="square-button" onClick={this.showResult}><h3>=</h3></div>
-         
+                <CalcInputField />
+                <CalcButtons />
             </div>
         )
     }
