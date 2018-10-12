f437a6acb008cc8e371c69ba5210e36ba7704649
Dmitry <manxxeta@gmail.com>
2018-10-12 15:41:02 -0700

    Updates

---
+++ precache-manifest.b35a52a2661a728d1c8762b086f0016e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f660a614e3bf58a8b97",
+    "url": "/react-playground-2/static/js/runtime~main.1f660a61.js"
+  },
+  {
+    "revision": "d87603c1816d2f9ea653",
+    "url": "/react-playground-2/static/js/main.d87603c1.chunk.js"
+  },
+  {
+    "revision": "8df7078f39324016d415",
+    "url": "/react-playground-2/static/js/1.8df7078f.chunk.js"
+  },
+  {
+    "revision": "d87603c1816d2f9ea653",
+    "url": "/react-playground-2/static/css/main.33cb3dfa.chunk.css"
+  },
+  {
+    "revision": "14aea2bf8b6ff151523e98e1adb3ee04",
+    "url": "/react-playground-2/index.html"
+  }
+];
