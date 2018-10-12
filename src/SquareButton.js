b252c8af109d87efdd9ec47e25b348c576114950
Dmitry <manxxeta@gmail.com>
2018-10-12 15:18:35 -0700

    in process

--- src/SquareButton.js
+++ src/SquareButton.js
@@ -5,7 +5,6 @@ import './index.css';
 class SquareButton extends React.Component {
 
     render(props) {
-
         return (
             <div className="square-button" onClick={this.props.onClick}>
             <h3>{this.props.symbol}</h3>
